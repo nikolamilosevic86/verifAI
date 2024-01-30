d85150d9d06d39d75e69a778c72ca27f9ab47eb8
Nikola Milosevic <nikola.milosevic@bayer.com>
2024-01-30 15:02:39 +0100

    Added button

--- client-gui/verifai-ui/src/App.js
+++ client-gui/verifai-ui/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <img className="App-logo" src={logo} alt="Logo"/>
       <div className="InputQuestion">
       <label className='QuestionLabel' for="question">Input your question in natural language</label><br/>
-      <input id="question" name="question" className="QuestionClass" placeholder="e.g. What genes are promising targets for prostate cancer?"></input>
+      <input id="question" name="question" className="QuestionClass" placeholder="e.g. What genes are promising targets for prostate cancer?"></input><button className='AskButton' role="button">Ask</button>
       </div>
     </div>
   );
