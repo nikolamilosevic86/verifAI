2c92e2bf59dce218eaf60469a38df38be8f314c0
LorenzoCassano <l.cassano25@studenti.uniba.it>
2024-04-18 17:54:45 +0200

    Finishing settings

--- client-gui/verifai-ui/src/MainScreen.js
+++ client-gui/verifai-ui/src/MainScreen.js
@@ -11,10 +11,13 @@ class MainScreen extends Component {
             modalOpen: false,
             value: "",
             temperature: 0,
+            lex_parameter:0.5,
+            sem_parameter:0.5,
             numDocuments: 10,
             startDate: "1940-01-01",
             endDate: "2030-01-01",
             search_type: "hybrid",
+            submitted: false,
             output: ""  // Holds HTML content safely
         };
         this.handleChange = this.handleChange.bind(this);
@@ -26,6 +29,8 @@ class MainScreen extends Component {
         this.handleStartDateChange = this.handleStartDateChange.bind(this);
         this.handleEndDateChange = this.handleEndDateChange.bind(this);
 
+        this.handleLexParamChange = this.handleLexParamChange.bind(this);
+        
         this.modalRef = React.createRef(); // Create a ref for the modal
         this.setWrapperRef = this.setWrapperRef.bind(this);             
         this.handleClickOutside = this.handleClickOutside.bind(this);
@@ -37,6 +42,15 @@ class MainScreen extends Component {
           modalOpen: !prevState.modalOpen
         }));
       };
+
+    
+    handleLexParamChange = (event) => {
+        const newValue = parseFloat(event.target.value);  // Ensure the value is treated as a number
+        this.setState({
+            lex_parameter: newValue,
+            sem_parameter: 1 - newValue  // Automatically calculate the complementary value
+        });
+    };
 
     handleStartDateChange = (event) => {
         this.setState({ startDate: event.target.value });
@@ -63,6 +77,7 @@ class MainScreen extends Component {
     
     clearOutput(){
         this.setState({ output: "" });
+        
     }
 
     
@@ -73,6 +88,8 @@ class MainScreen extends Component {
               startDate,
               endDate,
               search_type,
+              lex_parameter,
+              sem_parameter
              } = this.state;  // Using value from the state for the message
         const response = await fetch('http://3.74.47.54:5001/query/', {
             method: 'POST',
@@ -81,19 +98,43 @@ class MainScreen extends Component {
             },
             body: JSON.stringify({ query: value,
                                    search_type: search_type,
-                                   limit :numDocuments,
+                                   limit : numDocuments,
                                    filter_date_lte: endDate,
                                    filter_date_gte: startDate,
-                                   temperature: temperature
+                                   temperature: temperature,
+                                   lex_par:lex_parameter,
+                                   semantic_par:sem_parameter,
                                 })
         });
     
         const reader = response.body.getReader();
         const decoder = new TextDecoder('utf-8');
-    
+        const baseUrl = "https://pubmed.ncbi.nlm.nih.gov/";
+        const regex = /^\(\[\d+\]\)$/;
+        const regex1 = /^\(\[\d+\]\)\.$/;
+        const regex2 = /^\(\[\d+\]\)\;$/;
+        const regex3 = /^\(\[\d+\]\)\,$/;
+        const regex5 = /^\[\d+\]\.$/;
+        const regex6 = /^\[\d+\]$/;
+        const regex7 = /^\[\d+\]\,$/;
+        const regex8 = /^\[\d+\]\;$/;
+        const regex9 = /^\d+\)\.$/;
+        const regex10 = /^\(\d+\)\.$/;
+        const regex11 = /^\(\d+\)\$/;
+        
         const processResult = async (result) => {
             if (result.done) return;
             let token = decoder.decode(result.value, {stream: true});
+            console.log(token, regex.test(token), token.trim() !== token);
+            var no_space_token = token.trim()
+            if (regex.test(no_space_token) || regex1.test(no_space_token) || regex2.test(no_space_token) || regex3.test(no_space_token)
+                || regex5.test(no_space_token) || regex6.test(no_space_token) || regex7.test(no_space_token) || 
+                regex8.test(no_space_token) || regex9.test(no_space_token) || regex10.test(no_space_token) ||
+                regex11.test(no_space_token)) {
+                no_space_token = no_space_token.replace(/[\[\]()\.,;]/g, '');
+                var new_token = `<a href="${baseUrl + no_space_token}" target="_blank">${token}</a>`;
+                token = new_token;
+            }
             this.setOutput(token);
             await reader.read().then(processResult);
         };
@@ -102,13 +143,24 @@ class MainScreen extends Component {
     }
 
     handleChange(event) {
+        this.setState({ submitted: false });
         this.setState({ value: event.target.value });
     }
 
     handleSubmit(event) {
+        
         event.preventDefault();
+        this.setState({ submitted: true });
         this.clearOutput();
         this.sendMessage();
+
+        this.setState({temperature: 0,
+                      numDocuments: 10,
+                      startDate: "1940-01-01",
+                      endDate: "2030-01-01",
+                      search_type: "hybrid",
+                      lex_parameter:0.5,
+                      sem_parameter:0.5})
     }
 
     
@@ -153,12 +205,39 @@ class MainScreen extends Component {
                     <div className="ModalContent" ref={this.setWrapperRef}>
                     <h2>Search Configuration</h2>
                     <label>Type of Search: 
-                        <select value={this.state.searchType} onChange={this.handleSearchTypeChange}>
+                        <select value={this.state.search_type} onChange={this.handleSearchTypeChange}>
                             <option value="hybrid">Hybrid</option>
                             <option value="lexical">Lexical</option>
                             <option value="semantic">Semantic</option>
                         </select>
                     </label>
+
+                     
+                    {this.state.search_type === 'hybrid' && (
+                    <div className="temperature-labels">
+                        <label>
+                            Lexical Weights:{this.state.lex_parameter.toFixed(3)}, Semantic Weights:{(1-this.state.lex_parameter).toFixed(3)}
+                            <input 
+                                type="range" 
+                                min="0" 
+                                max="1" 
+                                value={this.state.lex_parameter}
+                                step="0.01"
+                                onChange={this.handleLexParamChange}
+                            />
+                            <button className="temperature-label start" onClick={() => this.setState({ lex_parameter: 0.3, sem_parameter: 0.7 })}>SEMANTIC</button>
+
+                            <button className="temperature-label middle" onClick={() => this.setState({ lex_parameter: 0.5, sem_parameter: 0.5 })}>NEUTRAL</button>
+
+                            <button className="temperature-label end" onClick={() => this.setState({ lex_parameter: 0.7, sem_parameter: 0.3 })}>LEXICAL</button>
+
+
+                        </label>
+                    </div>
+                    )}
+                    
+
+    
                         
                     <label>Number of Documents:
                     <select
@@ -237,11 +316,18 @@ class MainScreen extends Component {
                     
 
                     
+                    {this.state.submitted && (
+                        <div>
+                        <h2>{this.state.value}</h2> {}
+                        <div className="output-section">     
+                            <div className="output-tokens" dangerouslySetInnerHTML={{__html: this.state.output}} />
+                        </div>
+                        </div>
+                    )}
+
                     
 
-
-                                        
-                    <div className="OutputTokens" dangerouslySetInnerHTML={{__html: this.state.output}} />
+                  
                 </div>
             </div>
         );
