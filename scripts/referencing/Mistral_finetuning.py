2cc3739d8f3df037cd6a2f3aed830fe2b2e1219f
Bojana Bašaragin <bojana@lingvistika.org>
2024-07-11 16:10:08 +0200

    Final version of Mistral 2 fine-tuning
    Using PQAref dataset from Hugging face

--- scripts/referencing/Mistral_finetuning.py
+++ scripts/referencing/Mistral_finetuning.py
@@ -10,17 +10,14 @@ import torch
 import torch.nn as nn
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 import transformers
-import re
-import pandas as pd
-from datasets import Dataset, DatasetDict
-import json
 from peft import LoraConfig, get_peft_model
-from data_utils import tokenize_dataset, split
+from data_utils import tokenize_dataset
+from datasets import load_dataset
 
 
 
 model_name = "mistral"
-model_id = "mistralai/Mistral-7B-Instruct-v0.1" # "mistralai/Mistral-7B-Instruct-v0.2"
+model_id = "mistralai/Mistral-7B-Instruct-v0.2"
 
 model_file_name = model_name + "_standard_loss"
 
@@ -30,13 +27,13 @@ model = AutoModelForCausalLM.from_pretrained(
     model_id,
     trust_remote_code=True,
     quantization_config=quantization_config,
-    device_map='auto',
+    device_map='auto'
 )
 
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 tokenizer.pad_token = tokenizer.eos_token
-#print(tokenizer)
+
 
 
 
@@ -46,13 +43,13 @@ for param in model.parameters():
     param.requires_grad = False  
     if param.ndim == 1:
         param.data = param.data.to(torch.float32)
-        
+
 model.gradient_checkpointing_enable()
 model.enable_input_require_grads()
 
 class CastOutputToFloat(nn.Sequential):
     def forward(self, x): return super().forward(x).to(torch.float32)
-    
+
 model.lm_head = CastOutputToFloat(model.lm_head)
 
 config = LoraConfig(
@@ -80,88 +77,12 @@ print_trainable_parameters(model)
 
 
 
-# Dataset Preparation
-path = './merged_answers.json'
-with open(path, 'r', encoding="utf-8") as f:
-    documents = json.load(f)
-print(len(documents))
-    
-# Function to count occurrences of "abstract_id" in a document
-def count_abstract_id(document):
-    return document.count("\n\nabstract_id:")
-
-# Function to remove abstract_id in the GPT answer
-def abstract_id_in_answer(document):
-    if "abstract_id" in document:
-        document = re.sub('abstract_id', '', document)
-        document = re.sub('  ', ' ', document)
-    return document
-    
-
-def get_prepared_dataset(documents):
-    all_prepared_docs = []
-
-    for doc in documents:
-        inst = f"Instruction: {doc['query']}\n"
-
-        abstracts = doc['context']
-        abstracts = re.sub('Relevant abstracts: \r\n\r\n', 'Abstracts:\n\n', abstracts)
-        abstracts = re.sub('\r\n\r\n\r\n\r\n\r\n\r\nQuestion:[\s\S]*', '\n\n', abstracts) # remove Question        
-        abstracts = re.sub('\r\n\r\n\r\n', '\n\n', abstracts)
-        abstracts = re.sub('\r\n', '\n', abstracts) 
-
-        answer = doc['cleaned_answer']
-        answer = re.sub('\n\n', '\n', answer)
-        answer = abstract_id_in_answer(answer)
-        answer = f"Answer: {answer}"         
-
-        all_prepared_docs.append(inst + abstracts + answer)
-
-    # Removing documents with less than 10 abstracts
-    all_prepared_docs_filtered = []
-    for doc in all_prepared_docs:
-        if count_abstract_id(doc) > 9:  # menjati spram potreba 0, 1...
-            all_prepared_docs_filtered.append(doc)
-
-    instruct_idx_list = list(range(len(all_prepared_docs_filtered)))
-    
-    # Creating DataFrame
-    data_dict = {'input': all_prepared_docs_filtered, 'instruct_idx_list': instruct_idx_list}
-    df = pd.DataFrame.from_dict(data_dict)
-
-    return DatasetDict({"train": Dataset.from_pandas(df)})
-
-dataset = get_prepared_dataset(documents)
-#print(dataset)
-
-
-
-# Tokenization an conversion
+# Load dataset
+dataset = load_dataset("BojanaBas/PQAref")
 dataset = tokenize_dataset(dataset, tokenizer)
 dataset.set_format("torch")
-
-
-# Token input treshold
-max_length_threshold = 7000
-dataset = dataset.filter(lambda example: len(example["input_ids"]) <= max_length_threshold)
 print(dataset)
 
-# Train, val, test split: 80-10-10
-dataset = split(dataset)
-print(dataset)
-
-# Exporting datasets
-# train_dataset = dataset['train']
-# train_df = pd.DataFrame(train_dataset)
-# train_df.to_csv("Train_set.csv", encoding="utf-8")
-
-# val_dataset = dataset['val']
-# val_df = pd.DataFrame(val_dataset)
-# val_df.to_csv("Val_set.csv", encoding="utf-8")
-
-# test_dataset = dataset['test']
-# test_df = pd.DataFrame(test_dataset)
-# test_df.to_csv("Test_set.csv", encoding="utf-8")
 
 
 
@@ -184,7 +105,9 @@ training_arguments = transformers.TrainingArguments(
     logging_dir="logs/" + model_file_name,
     load_best_model_at_end=True,
     metric_for_best_model="eval_loss",
-    save_steps=400
+    save_steps=400,
+    report_to=[]
+    
 )
 
 trainer = transformers.Trainer(
