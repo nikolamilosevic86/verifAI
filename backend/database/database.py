dd704de142588b6812e94b2b196726e8550af89f
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-09-21 11:29:02 +0200

    Added insert_question function

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,6 +1,6 @@
 import asyncpg
 import json
-
+import datetime
 
 class Database:
     """
@@ -61,6 +61,16 @@ class Database:
         print("Result = ",result)
         await self.close()
         return result['id']
+
+    async def insert_question(self, username: str, question: str):
+        current_timestamp = datetime.datetime.now()
+        await self.open()
+        await self.sql_database.execute(
+            'INSERT INTO user_questions (username, question, question_date) VALUES ($1, $2, $3)', username, question, current_timestamp
+        )
+        await.self_close()
+        
+
 
     async def get_web_session(self, session_id):
         await self.open()
